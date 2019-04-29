376329da9a3d25ce8851224e0e1e30b274ba6297
meetshashank007 <meet.shashank007@gmail.com>
2019-04-29 22:56:47 +0530

    final profile

--- devconnector/routes/api/profile.js
+++ devconnector/routes/api/profile.js
@@ -218,4 +218,70 @@ routes.post(
   }
 );
 
+//@route    DELETE api/profile/education/:edu_id
+//@desc     Delete education form profile
+//@access   Private
+routes.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //Get remove Index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        //Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        //Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+//@route    DELETE api/profile/experiance/:exp_id
+//@desc     Delete experiance form profile
+//@access   Private
+routes.delete(
+  "/experiance/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        //Get remove Index
+        const removeIndex = profile.experiance
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        //Splice out of array
+        profile.experiance.splice(removeIndex, 1);
+
+        //Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+//@route    DELETE api/profile
+//@desc     Delete user profile
+//@access   Private
+routes.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+      .then(() => {
+        //User
+        User.findOneAndRemove({ _id: req.user.id }).then(() =>
+          res.json({ success: true })
+        );
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = routes;
