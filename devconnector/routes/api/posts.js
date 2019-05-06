835c508a98754c2bedb31e7b322b18572ed3e2b6
meetshashank007 <meet.shashank007@gmail.com>
2019-05-06 23:29:06 +0530

    delete posts

--- devconnector/routes/api/posts.js
+++ devconnector/routes/api/posts.js
@@ -6,6 +6,7 @@ const passport = require("passport");
 
 //Load modal
 const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 //Load Validation
 const validatePostInput = require("../../validation/post");
@@ -39,6 +40,59 @@ routes.post(
       .save()
       .then(post => res.json(post))
       .catch(err => res.status(400).json(err));
+  }
+);
+
+//@route    GET api/posts
+//@desc     Get all posts
+//@access   Public
+routes.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nopostsfound: "no post found with the ID" })
+    );
+});
+
+//@route    GET api/posts/:id
+//@desc     Get posts by id
+//@access   Public
+routes.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(404).json({ nopostsfound: "no post found with the ID" })
+    );
+});
+
+//@route    DELETE api/posts/:id
+//@desc     Delete the target Post
+//@access   Private
+routes.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //Chekc for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          //Delete
+          post
+            .remove()
+            .then(() => res.json({ success: true }))
+            .catch(err =>
+              res.status(404).json({ nopostsfound: "no post found" })
+            );
+        })
+        .catch(err => res.status(404).json({ nopostsfound: "no post found" }));
+    });
   }
 );
 
