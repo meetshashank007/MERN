77f69f64e4dc59c90be91d64392cabe3b6a46a10
meetshashank007 <meet.shashank007@gmail.com>
2019-05-19 17:52:27 +0530

    react client initial commit

---
+++ devconnector/client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
